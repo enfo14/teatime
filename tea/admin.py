676f837ac21190013500d030c19842427a0e9897
Enric Fortin <enric.fortin@cfms.org.uk>
2020-07-07 15:10:21 +0100

    Add TeaRoundAdmin class

--- tea/admin.py
+++ tea/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from tea.models import Profile
+from tea.models import Profile, TeaRound
 
 
 class ProfileInline(admin.StackedInline):
@@ -15,5 +15,10 @@ class UserAdmin(BaseUserAdmin):
     inlines = (ProfileInline,)
 
 
+class TeaRoundAdmin(admin.ModelAdmin):
+    model = TeaRound
+
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(TeaRound, TeaRoundAdmin)
