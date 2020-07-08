0787ca233690fbc21d61372d56a0265cbc88b5c3
Enric Fortin <enric.fortin@cfms.org.uk>
2020-07-08 11:18:04 +0100

    Further amends to models

--- tea/models.py
+++ tea/models.py
@@ -9,10 +9,11 @@ class UserLevel(models.IntegerChoices):
 
 
 class Member(models.Model):
-    id = models.UUIDField(default=uuid4, primary_key=True)
+    id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     level = models.IntegerField(choices=UserLevel.choices, default=UserLevel.JUNIOR)
+    deleted = models.BooleanField(default=False)
 
 
 class TeaRound(models.Model):
