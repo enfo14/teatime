a0f2f13a4832d465aba2f20b30326dbc596b3125
Enric Fortin <enric.fortin@cfms.org.uk>
2020-07-12 22:40:00 +0100

    Add tests for confirmation-dialog

--- frontend/src/app/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ frontend/src/app/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -5,6 +5,7 @@ import { ConfirmationDialog } from './confirmation-dialog.component';
 import { AppState } from '../app.reducers';
 import { initialState } from '../store/tea.state';
 import { Store } from '@ngrx/store';
+import { TeaTimeActions } from '../store/tea.actions';
 
 describe('ConfirmationDialog', () => {
   let component: ConfirmationDialog;
@@ -23,10 +24,66 @@ describe('ConfirmationDialog', () => {
     fixture = TestBed.createComponent(ConfirmationDialog);
     component = fixture.componentInstance;
     store = TestBed.get(Store);
+    spyOn(store, 'dispatch');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('open', () => {
+    it('sets the \'active\' attribute to true', () => {
+      expect(component.active).toBeFalse();
+      component.open();
+      expect(component.active).toBeTrue();
+    });
+  });
+
+  describe('close', () => {
+    beforeEach(() => {
+      component.active = true;
+    });
+
+    it('sets the \'active\' attribute to false', () => {
+      expect(component.active).toBeTrue();
+      component.close();
+      expect(component.active).toBeFalse();
+    });
+  });
+
+  describe('onYesClick', () => {
+    beforeEach(() => {
+      component.action = TeaTimeActions.VoidTeaRound;
+      spyOn(component, 'close').and.callThrough();
+    });
+
+    it('should dispatch the action that has been passed as input', () => {
+      component.onYesClick();
+      expect(store.dispatch).toHaveBeenCalledTimes(1);
+      expect(store.dispatch).toHaveBeenCalledWith(TeaTimeActions.VoidTeaRound())
+    });
+
+    it('should close the dialog', () => {
+      component.onYesClick();
+      expect(component.close).toHaveBeenCalled();
+    });
+  });
+
+  describe('onNoClick', () => {
+    beforeEach(() => {
+      component.action = TeaTimeActions.VoidTeaRound;
+      spyOn(component, 'close').and.callThrough();
+    });
+
+    it('should not dispatch any action', () => {
+      component.onNoClick();
+      expect(store.dispatch).not.toHaveBeenCalled();
+    });
+
+    it('should close the dialog', () => {
+      component.onNoClick();
+      expect(component.close).toHaveBeenCalled();
+    });
   });
 });
