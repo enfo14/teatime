fadc265263d69dac0250665fbdcc558644e48d51
Enric Fortin <enric.fortin@cfms.org.uk>
2020-07-12 21:37:58 +0100

    Add CORS headers

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -24,10 +24,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "tea.apps.TeaConfig",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -82,3 +84,4 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
+CORS_ORIGIN_ALLOW_ALL = True
