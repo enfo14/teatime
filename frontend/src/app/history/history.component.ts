65685fc27f42fc632c597abc083be84ef598e328
Enric Fortin <enric.fortin@cfms.org.uk>
2020-07-13 12:56:50 +0100

    Add history component and styles

--- frontend/src/app/history/history.component.ts
+++ frontend/src/app/history/history.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, forkJoin, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as moment from 'moment';
+
+import { AppState } from '../app.reducers';
+import { TeaTimeActions } from '../store/tea.actions';
+import * as fromTeaTime from '../store/tea.selectors'
+import { TeaMaker, Level, Tea } from '../teatime.models';
 
 @Component({
   selector: 'app-history',
@@ -7,9 +16,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryComponent implements OnInit {
 
-  constructor() { }
+  makers$: Observable<TeaMaker[]>;
+  topMakers$: Observable<any[]>;
+  history$: Observable<Tea[]>;
+  lastWeek$: Observable<Tea[]>;
 
-  ngOnInit(): void {
-  }
+  constructor(private store: Store<AppState>) { }
 
+  ngOnInit() {
+    this.store.dispatch(TeaTimeActions.GetTeaHistory());
+    this.store.dispatch(TeaTimeActions.GetTeaMakers());
+    this.history$ = this.store.select(fromTeaTime.selectHistory);
+    this.makers$ = this.store.select(fromTeaTime.selectMakers);
+
+    const timestamp: moment.Moment = moment()
+      .subtract(7, 'days');
+
+    this.lastWeek$ = this.history$.pipe(
+      map(history => history.filter(tea => moment(tea.timestamp) > timestamp))
+    )
+
+    this.topMakers$ = combineLatest(this.history$, this.makers$, (history, makers) => {
+      return makers.map(maker => ({
+        ...maker,
+        rounds: history.filter(tea => tea.made_by.id === maker.id)
+      }))
+        .sort((a, b) => b.rounds.length - a.rounds.length)
+    });
+  };
 }
